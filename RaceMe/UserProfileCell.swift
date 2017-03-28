2e2a0330c4d8991826f8320351fe43b8413754d0
thanhluu <thanhlt90@gmail.com>
2017-03-28 21:04:31 +0700

    Update Profile Cell

--- RaceMe/UserProfileCell.swift
+++ RaceMe/UserProfileCell.swift
@@ -14,6 +14,13 @@ class UserProfileCell: UITableViewCell, UIScrollViewDelegate {
     @IBOutlet weak var userStatisticsScrollView: UIScrollView!
     @IBOutlet weak var userStatisticsPageControl: UIPageControl!
     
+    let userStats1 = ["title":"Kilometers", "current_period":"8.3", "last_period":"0"]
+    let userStats2 = ["title":"Average Pace (Min/Km)", "current_period":"1:15:08", "last_period":"2:33:54"]
+    let userStats3 = ["title":"Activities", "current_period":"2", "last_period":"1"]
+    let userStats4 = ["title":"Time Spent", "current_period":"31:52", "last_period":"04:50"]
+    
+    var userStatisticsArray = [Dictionary<String,String>]()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -25,9 +32,25 @@ class UserProfileCell: UITableViewCell, UIScrollViewDelegate {
         
         // Config Scroll View
         userStatisticsScrollView.isPagingEnabled = true
-        userStatisticsScrollView.contentSize = CGSize(width: UIScreen.main.bounds.width * 4, height: 120)
-        userStatisticsScrollView.showsHorizontalScrollIndicator = true
+        userStatisticsScrollView.contentSize = CGSize(width: frame.width * 4, height: 120)
+        userStatisticsScrollView.showsHorizontalScrollIndicator = false
         userStatisticsScrollView.delegate = self
+        
+        loadUserStatistics()
+    }
+    
+    func loadUserStatistics() {
+        userStatisticsArray = [userStats1,userStats2,userStats3,userStats4]
+        for (index, userStatistics) in userStatisticsArray.enumerated() {
+            if let userStatisticsView = Bundle.main.loadNibNamed("UserStatistics", owner: self, options: nil)?.first as? UserStatisticsView {
+                userStatisticsView.titleLabel.text = userStatistics["title"]
+                userStatisticsView.currentPeriodCount.text = userStatistics["current_period"]
+                userStatisticsView.lastPeriodCount.text = userStatistics["last_period"]
+                userStatisticsScrollView.addSubview(userStatisticsView)
+                userStatisticsView.frame.size.width = frame.width
+                userStatisticsView.frame.origin.x = CGFloat(index) * frame.width
+            }
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
