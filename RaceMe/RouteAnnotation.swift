4b1db83b6839a66380d465ee4f70870bb7e511f8
Vu Long <vulong264@gmail.com>
2017-04-01 18:43:09 +0700

    avatar displayed on pin, tap to open routedetailVC

--- RaceMe/RouteAnnotation.swift
+++ RaceMe/RouteAnnotation.swift
@@ -8,7 +8,35 @@
 
 import UIKit
 import MapKit
-class RouteAnnotation: MKPointAnnotation {
+class RouteAnnotation: MKAnnotationView {
+    static let PIN_DISTANCE = "DISTANCE"
+    static let PIN_EVENT = "EVENT"
     var pinCustomImage: String!
+    var pinUsername: String!
     var pinCustomTitle: String!
+    var pinType = PIN_DISTANCE
+    var distance = 0
+    var personCount = 0
+    let dateFormatter = DateFormatter()
+    let dateFormat = "hh:mm a"
+    var imageView = UIImage()
+    var title: String!
+    
+    //    var startLoc: CLLocationCoordinate2D
+    func setTitleEvent(scheduled: Date?){
+        dateFormatter.dateFormat = dateFormat
+        let dateString = dateFormatter.string(from: scheduled!)
+        if personCount > 1 {
+            self.title = "\(pinUsername!) and \(personCount-1) person at \(dateString)"
+        } else {
+            self.title = "\(pinUsername!) will run at \(dateString)"
+        }
+    }
+    //    var startLoc: CLLocationCoordinate2D
+    func setTitleDistance(){
+        self.title = "\(self.distance) km"
+    }
+}
+class RoutePoint: MKPointAnnotation{
+    var AnnoView = MKAnnotationView()
 }
