f53ba5df8fdf87e40473891e06267a8258cda0ba
Vu Long <vulong264@gmail.com>
2017-04-10 18:38:46 +0700

    initial commit

---
+++ RaceMe/ChallengeViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ChallengeViewCell.swift
+//  RaceMe
+//
+//  Created by LVMBP on 4/10/17.
+//  Copyright © 2017 CoderSchool. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeViewCell: UITableViewCell {
+
+    @IBOutlet weak var chalImage: UIImageView!
+    @IBOutlet weak var chalNameLabel: UILabel!
+    @IBOutlet weak var creatorLabel: UILabel!
+    @IBOutlet weak var groupLabel: UILabel!
+    @IBOutlet weak var fromDateLabel: UILabel!
+    @IBOutlet weak var toDateLabel: UILabel!
+
+    
+    @IBOutlet weak var totWOLabel: UILabel!
+    
+    @IBOutlet weak var totDistLabel: UILabel!
+    @IBOutlet weak var weeklyWOLabel: UILabel!
+    @IBOutlet weak var weeklyDistLabel: UILabel!
+    @IBOutlet weak var miWODistLabel: UILabel!
+    
+    @IBOutlet weak var minWOPaceLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
